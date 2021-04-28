47d87cf05aa24bbfbff3e5aae3db90e52f023f3a
linruikai <949296157@qq.com>
2021-04-28 16:14:48 +0800

    2021/04/28 周三 16:14:48.23

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 d:
 cd D:\文件\学习资料\git脚本\test\src\test\java\com\ruoyi\console
 set now=%date% %time%
-echo '12331' > ConsoleApplicationTests.java
+echo now > ConsoleApplicationTests.java
 cd D:\文件\学习资料\git脚本\test
 git add .
 git commit -m "%now%"
